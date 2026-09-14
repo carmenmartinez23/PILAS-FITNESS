b110f2c27da8b72cade55c64ab9b853f84962a3b
Carmen Martínez <cmvos97@gmail.com>
2026-09-14 11:06:14 +0200

    Update Firebase config with databaseURL
    Added databaseURL to Firebase configuration.

--- static/firebase-config.js
+++ static/firebase-config.js
@@ -1,10 +1,22 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
+
   apiKey: "AIzaSyB89tIVWX9hdZwr8QStD7sApaY41i-F5xs",
+
   authDomain: "pilas-fitness.firebaseapp.com",
+
+  databaseURL: "https://pilas-fitness-default-rtdb.europe-west1.firebasedatabase.app",
+
   projectId: "pilas-fitness",
+
   storageBucket: "pilas-fitness.firebasestorage.app",
+
   messagingSenderId: "612445381929",
+
   appId: "1:612445381929:web:0e36a0e90935d340474338",
+
   measurementId: "G-WMB61LHBK1"
+
 };
 export { firebaseConfig };
