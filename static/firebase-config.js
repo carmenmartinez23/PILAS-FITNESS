530cd55f38c485a33ea8418abd84dcdf6df8c5dc
Carmen Martínez <cmvos97@gmail.com>
2026-09-18 13:01:09 +0200

    Update firebase-config.js

--- static/firebase-config.js
+++ static/firebase-config.js
@@ -1,6 +1,8 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB89tIVWX9hdZwr8QStD7sApaY41i-F5xs",
   authDomain: "pilas-fitness.firebaseapp.com",
+  databaseURL: "https://pilas-fitness-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "pilas-fitness",
   storageBucket: "pilas-fitness.firebasestorage.app",
   messagingSenderId: "612445381929",
